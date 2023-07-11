c045b3405043f642ed6fbbed9373b6e0c454370a
Ousmane Coulibaly <hticnetworks@gmail.com>
2023-07-11 12:22:11 +0000

    Add Validate with joi

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,15 @@
 const authRoutes = require('express').Router()
 const { register, login, logout, getMe, refreshTokens, changePassword } = require('../controllers/auth/Auth.Controller');
 const { protect } = require('../middleware/authMiddleware');
+const { loginValidator, changePasswordValidator, registerValidator } = require('../validator/authValidator');
 
 
 // Auth Routes
-authRoutes.post('/register', register);
-authRoutes.post('/login', login);
+authRoutes.post('/register', registerValidator, register);
+authRoutes.post('/login',loginValidator, login);
 authRoutes.post('/logout', logout);
 authRoutes.get('/profile', protect, getMe);
 authRoutes.post('/refresh-token', protect, refreshTokens);
-authRoutes.put('/change-password', protect, // userValidator.changePasswordValidator,
-    changePassword,
-);
+authRoutes.put('/change-password', protect, changePasswordValidator, changePassword,);
 
 module.exports = authRoutes;
